ef72dcf708e0e27782b0e0501d2ef05d7448c687
Lasha <amashukeli.lasha@gmail.com>
2015-12-19 01:35:12 +0400

    Added device scanner

---
+++ hackTbilisi2015.Core/BusinessObjects/iBeacon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackTbilisi2015.Core.BusinessObjects
+{
+    public class iBeacon
+    {
+        public string UUID { get; set; }
+        public ushort Major { get; set; }
+        public ushort Minor { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Key { get; set; }
+        public string MacAddress { get; set; }
+    }
+}
--- hackTbilisi2015/MainActivity.cs
+++ hackTbilisi2015/MainActivity.cs
@@ -1,26 +1,69 @@
 using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Bluetooth;
+using Android.Content;
+using System;
 
 namespace hackTbilisi2015
 {
-	[Activity (Label = "hackTbilisi2015", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "hackTbilisi2015", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity, BluetoothAdapter.ILeScanCallback
+    {
 
-		protected override void OnCreate (Bundle savedInstanceState)
-		{
-			Xamarin.Insights.Initialize (XamarinInsights.ApiKey, this);
-			base.OnCreate (savedInstanceState);
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            var adapter = BluetoothAdapter.DefaultAdapter;
+
+            adapter.StartLeScan(this);
+          
+        }
+
+        public void OnLeScan(BluetoothDevice device, int rssi, byte[] scanRecord)
+        {
+            //device.CreateBond();
+            var res = device.FetchUuidsWithSdp();
+            var uids = device.GetUuids();
+
+            int startByte = 2;
+            var patternFound = false;
+            while (startByte <= 5)
+            {
+                if (((int)scanRecord[startByte + 2] & 0xff) == 0x02 && //Identifies an iBeacon
+                        ((int)scanRecord[startByte + 3] & 0xff) == 0x15)
+                { //Identifies correct data length
+                    patternFound = true;
+                    break;
+                }
+                startByte++;
+            }
+
+            if (patternFound)
+            {
+                //Convert to hex String
+                byte[] uuidBytes = new byte[16];
+                //System.arraycopy(scanRecord, startByte + 4, uuidBytes, 0, 16);
+                Array.Copy(scanRecord, startByte + 4, uuidBytes, 0, 16);
+                var hexString = BitConverter.ToString(uuidBytes).Replace("-","");
+
+                //Here is your UUID
+                var uuid = hexString.Substring(0, 8) + "-" +
+                        hexString.Substring(8, 4) + "-" +
+                        hexString.Substring(12, 4) + "-" +
+                        hexString.Substring(16, 4) + "-" +
+                        hexString.Substring(20, 12);
+
+                //Here is your Major value
+                int major = (scanRecord[startByte + 20] & 0xff) * 0x100 + (scanRecord[startByte + 21] & 0xff);
+
+                //Here is your Minor value
+                int minor = (scanRecord[startByte + 22] & 0xff) * 0x100 + (scanRecord[startByte + 23] & 0xff);
+            }
+        }
+
+     
+    }
 }
