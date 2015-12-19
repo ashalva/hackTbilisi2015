914eabaf1db6d9224a10c5b258b90db50f0afddb
BUKA <avanashvili5@yahoo.com>
2015-12-19 12:28:56 +0400

    implementing main activity  ui

--- hackTbilisi2015/Activities/MainActivity.cs
+++ hackTbilisi2015/Activities/MainActivity.cs
@@ -9,6 +9,7 @@ using hackTbilisi2015.Core.BusinessObjects;
 using System.Linq;
 using System.Timers;
 using Newtonsoft.Json;
+using Java.Lang;
 
 namespace hackTbilisi2015.Activities
 {
@@ -16,120 +17,132 @@ namespace hackTbilisi2015.Activities
 	public class MainActivity : Activity, BluetoothAdapter.ILeScanCallback
 	{
 		private List<iBeacon> _beacons;
-        int beaconCount = 4;
-        BluetoothAdapter adapter;
-        DateTime startDate;
-        Timer timer;
-        int elapsedSeconds;
-        TextView text;
-        Button button;
-        bool activityWasStarted = false;
-        protected override void OnCreate (Bundle savedInstanceState)
+		int beaconCount = 4;
+		BluetoothAdapter adapter;
+		Timer timer;
+		int elapsedSeconds;
+		Button _findTheBeacons, _scan;
+		bool activityWasStarted = false;
+		ProgressDialog _progressDialog;
+
+		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 			SetContentView (Resource.Layout.Main);
-            text = FindViewById<TextView>(Resource.Id.textView1);
-            text.Text = "";
-            _beacons = new List<iBeacon>();
-            adapter = BluetoothAdapter.DefaultAdapter;
-            startDate = DateTime.Now;
-            timer = new Timer();
-            timer.Elapsed += Timer_Elapsed;
-            timer.Interval = 1000;
-            elapsedSeconds = 0;
-            timer.Start();
-			adapter.StartLeScan (this);
+			_scan = FindViewById<Button> (Resource.Id.scan);
 
-            button = FindViewById<Button>(Resource.Id.myButton);
-            button.Click += delegate
-            {
-                timer.Stop();
-                activityWasStarted = true;
-                var intent = new Intent(this, typeof(SearchActivity));
-                var serializedBeacons = JsonConvert.SerializeObject(_beacons);
-                intent.PutExtra("Beacons", serializedBeacons);
-                StartActivity(intent);
-            };
+			timer = new Timer ();
+			timer.Elapsed += Timer_Elapsed;
+			timer.Interval = 1000;
+			adapter = BluetoothAdapter.DefaultAdapter;
+
+			_beacons = new List<iBeacon> ();
+			_scan.Click += (object sender, EventArgs e) => {
+				_progressDialog = ProgressDialog.Show (this, "Please wait...", "We are scanning the beacons", true);
+				_beacons.Clear ();
+				_scan.Enabled = false;
+				elapsedSeconds = 0;
+				timer.Start ();
+				adapter.StartLeScan (this);
+			};
+
+			_findTheBeacons = FindViewById<Button> (Resource.Id.myButton);
+			_findTheBeacons.Enabled = false;
+			_findTheBeacons.SetTextColor (Android.Graphics.Color.DarkGray);
+			_findTheBeacons.Click += delegate {
+				timer.Stop ();
+				activityWasStarted = true;
+				var intent = new Intent (this, typeof(SearchActivity));
+				var serializedBeacons = JsonConvert.SerializeObject (_beacons);
+				intent.PutExtra ("Beacons", serializedBeacons);
+				StartActivity (intent);
+			};
 		}
 
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            elapsedSeconds++;
-            if (elapsedSeconds > 10)
-            {
-                timer.Stop();
-                adapter.StopLeScan(this);
-                elapsedSeconds = 0;
-                if (_beacons.Count< beaconCount)
-                    RunOnUiThread(() => Toast.MakeText(this, $"{_beacons.Count} ვიპოვე {beaconCount}-დან", ToastLength.Long).Show());
-            }
-        }
-
-        public void OnLeScan (BluetoothDevice device, int rssi, byte[] scanRecord)
+		private void Timer_Elapsed (object sender, ElapsedEventArgs e)
 		{
-            if (!_beacons.Any(x => x.MacAddress == device.Address))
-            {
-                int startByte = 2;
-                var patternFound = false;
-                while (startByte <= 5)
-                {
-                    if (((int)scanRecord[startByte + 2] & 0xff) == 0x02 &&
-                        ((int)scanRecord[startByte + 3] & 0xff) == 0x15)
-                    { //Identifies correct data length
-                        patternFound = true;
-                        break;
-                    }
-                    startByte++;
-                }
+			elapsedSeconds++;
+			if (elapsedSeconds > 10) {
+				timer.Stop ();
+				adapter.StopLeScan (this);
+				elapsedSeconds = 0;
+				if (_beacons.Count < beaconCount) {
+					RunOnUiThread (() => {
+						if (_progressDialog.IsShowing) {
+							_progressDialog.Hide ();
+							_progressDialog.Dismiss ();
+						}
+						Toast.MakeText (this, string.Format ("{0} ვიპოვე {1}-დან", _beacons.Count, beaconCount),
+							ToastLength.Long).Show ();
+						_scan.Enabled = true;
+						_findTheBeacons.Enabled = true;
+						_findTheBeacons.SetTextColor (Android.Graphics.Color.White);
+					});
+				}
+			}
+		}
 
-                if (patternFound)
-                {
-                    //Convert to hex String
-                    byte[] uuidBytes = new byte[16];
-                    Array.Copy(scanRecord, startByte + 4, uuidBytes, 0, 16);
-                    var hexString = BitConverter.ToString(uuidBytes).Replace("-", "");
+		public void OnLeScan (BluetoothDevice device, int rssi, byte[] scanRecord)
+		{
+			if (!_beacons.Any (x => x.MacAddress == device.Address)) {
+				int startByte = 2;
+				var patternFound = false;
+				while (startByte <= 5) {
+					if (((int)scanRecord [startByte + 2] & 0xff) == 0x02 &&
+					    ((int)scanRecord [startByte + 3] & 0xff) == 0x15) { //Identifies correct data length
+						patternFound = true;
+						break;
+					}
+					startByte++;
+				}
 
-                    //Here is your UUID
-                    var uuid = hexString.Substring(0, 8) + "-" +
-                               hexString.Substring(8, 4) + "-" +
-                               hexString.Substring(12, 4) + "-" +
-                               hexString.Substring(16, 4) + "-" +
-                               hexString.Substring(20, 12);
+				if (patternFound) {
+					//Convert to hex String
+					byte[] uuidBytes = new byte[16];
+					Array.Copy (scanRecord, startByte + 4, uuidBytes, 0, 16);
+					var hexString = BitConverter.ToString (uuidBytes).Replace ("-", "");
 
-                    //Here is your Major value
-                    int major = (scanRecord[startByte + 20] & 0xff) * 0x100 + (scanRecord[startByte + 21] & 0xff);
+					//Here is your UUID
+					var uuid = hexString.Substring (0, 8) + "-" +
+					           hexString.Substring (8, 4) + "-" +
+					           hexString.Substring (12, 4) + "-" +
+					           hexString.Substring (16, 4) + "-" +
+					           hexString.Substring (20, 12);
 
-                    //Here is your Minor value
-                    int minor = (scanRecord[startByte + 22] & 0xff) * 0x100 + (scanRecord[startByte + 23] & 0xff);
+					//Here is your Major value
+					int major = (scanRecord [startByte + 20] & 0xff) * 0x100 + (scanRecord [startByte + 21] & 0xff);
 
-                    if (!_beacons.Any(b => b.UUID == uuid && b.Major == major && b.Minor == minor))
-                        _beacons.Add(new iBeacon()
-                        {
-                            UUID = uuid,
-                            Minor = Convert.ToUInt16(minor),
-                            Major = Convert.ToUInt16(major),
-                            Name = device.Name,
-                            MacAddress = device.Address
-                        });
-                }
-                if (_beacons.Count == beaconCount)
-                {
-                    Toast.MakeText(this, $"ვიპოვე {beaconCount} ცალი.", ToastLength.Long).Show();
-                    adapter.StopLeScan(this);
-                    timer.Stop();
-                    elapsedSeconds = 0;
-                    foreach (var beacon in _beacons)
-                    {
-                        text.Text += beacon.Name;
-                    }
-                    if (!activityWasStarted)
-                    {
-                       
-                    }
-                }
-            }
+					//Here is your Minor value
+					int minor = (scanRecord [startByte + 22] & 0xff) * 0x100 + (scanRecord [startByte + 23] & 0xff);
+
+					if (!_beacons.Any (b => b.UUID == uuid && b.Major == major && b.Minor == minor))
+						_beacons.Add (new iBeacon () {
+							UUID = uuid,
+							Minor = Convert.ToUInt16 (minor),
+							Major = Convert.ToUInt16 (major),
+							Name = device.Name,
+							MacAddress = device.Address
+						});
+				}
+				if (_beacons.Count == beaconCount) {
+					if (_progressDialog.IsShowing) {
+						_progressDialog.Hide ();
+						_progressDialog.Dismiss ();
+					}
+					Toast.MakeText (this, string.Format ("ვიპოვე {0} ცალი.", beaconCount), ToastLength.Short).Show ();
+					_findTheBeacons.Enabled = true;
+					_findTheBeacons.SetTextColor (Android.Graphics.Color.White);
+					_scan.Enabled = true;
+					adapter.StopLeScan (this);
+					timer.Stop ();
+					elapsedSeconds = 0;
+//					foreach (var beacon in _beacons) {
+//						text.Text += beacon.Name;
+//					}
+				}
+			}
 		}
 
 
-    }
+	}
 }
--- hackTbilisi2015/Activities/SearchActivity.cs
+++ hackTbilisi2015/Activities/SearchActivity.cs
@@ -15,103 +15,102 @@ using hackTbilisi2015.Helpers;
 
 namespace hackTbilisi2015.Activities
 {
-    [Activity(Label = "SearchActivity")]
-    public class SearchActivity : Activity, IBeaconConsumer
-    {
-        IBeaconManager _iBeaconManager;
-        RangeNotifier _rangeNotifier;
-        List<Region> _rangingRegion;
-        List<BeaconsInfo> _beaconsInfo;
-        private List<iBeacon> _beacons;
-        int foundBeaconsCount = 0;
-        TextView foundText;
-        TextView proximityText;
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.Search);
-            foundText = FindViewById<TextView>(Resource.Id.found);
-            foundText.Text = $"ნაპოვნია: {foundBeaconsCount}";
-            proximityText = FindViewById<TextView>(Resource.Id.proximity);
-            proximityText.Text = "";
-            var json = Intent.GetStringExtra("Beacons");
-            _beacons = JsonConvert.DeserializeObject<List<iBeacon>>(json);
-            _beaconsInfo = new List<BeaconsInfo>();
-            _iBeaconManager = IBeaconManager.GetInstanceForApplication(this);
+	[Activity (Label = "SearchActivity")]
+	public class SearchActivity : Activity, IBeaconConsumer
+	{
+		IBeaconManager _iBeaconManager;
+		RangeNotifier _rangeNotifier;
+		List<Region> _rangingRegion;
+		List<BeaconsInfo> _beaconsInfo;
+		private List<iBeacon> _beacons;
+		int foundBeaconsCount = 0;
+		TextView foundText;
+		TextView proximityText;
 
-            _iBeaconManager.Bind(this);
-            _rangingRegion = new List<Region>();
-            _rangeNotifier = new RangeNotifier();
-            _rangeNotifier.DidRangeBeaconsInRegionComplete += DidRange;
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.Search);
+			foundText = FindViewById<TextView> (Resource.Id.found);
+			foundText.Text = $"ნაპოვნია: {foundBeaconsCount}";
+			proximityText = FindViewById<TextView> (Resource.Id.proximity);
+			proximityText.Text = "";
+			var json = Intent.GetStringExtra ("Beacons");
+			_beacons = JsonConvert.DeserializeObject<List<iBeacon>> (json);
+			_beaconsInfo = new List<BeaconsInfo> ();
+			_iBeaconManager = IBeaconManager.GetInstanceForApplication (this);
 
-            foreach (var beacon in _beacons)
-            {
-                _rangingRegion.Add(new Region(beacon.Name, beacon.UUID, null, null));
-                _beaconsInfo.Add(new BeaconsInfo { Beacon = beacon, WasFound = false, ProximityType = ProximityType.Unknown });
-            }
-        }
+			_iBeaconManager.Bind (this);
+			_rangingRegion = new List<Region> ();
+			_rangeNotifier = new RangeNotifier ();
+			_rangeNotifier.DidRangeBeaconsInRegionComplete += DidRange;
 
-        public void OnIBeaconServiceConnect()
-        {
-            _iBeaconManager.SetRangeNotifier(_rangeNotifier);
-            foreach (var region in _rangingRegion)
-            {
-                _iBeaconManager.RangedRegions.Add(region);
-                _iBeaconManager.StartRangingBeaconsInRegion(region);
-            }
-        }
+			foreach (var beacon in _beacons) {
+				_rangingRegion.Add (new Region (beacon.Name, beacon.UUID, null, null));
+				_beaconsInfo.Add (new BeaconsInfo {
+					Beacon = beacon,
+					WasFound = false,
+					ProximityType = ProximityType.Unknown
+				});
+			}
+		}
 
-        void DidRange(object sender, RangeEventArgs e)
-        {
-            if (e.Beacons.Count > 0)
-            {
-                foreach (var beacon in e.Beacons)
-                {
-                    var currentBeaconInfo = _beaconsInfo.FirstOrDefault(x => x.Beacon.UUID.ToLower() == beacon.ProximityUuid && x.Beacon.Major == beacon.Major && x.Beacon.Minor == beacon.Minor);
-                    if (!(currentBeaconInfo == null) && !currentBeaconInfo.WasFound)
-                        switch ((ProximityType)beacon.Proximity)
-                        {
-                            case ProximityType.Immediate:
-                                currentBeaconInfo.ProximityType = ProximityType.Immediate;
-                                break;
-                            case ProximityType.Near:
-                                currentBeaconInfo.ProximityType = ProximityType.Near;
-                                break;
-                            case ProximityType.Far:
-                                currentBeaconInfo.ProximityType = ProximityType.Far;
-                                break;
-                            case ProximityType.Unknown:
-                                currentBeaconInfo.ProximityType = ProximityType.Unknown;
-                                break;
-                        }
-                }
-                var nearest = _beaconsInfo.Where(x => !x.WasFound && !(x.ProximityType == ProximityType.Unknown)).OrderBy(x => (int)x.ProximityType).FirstOrDefault();
-                if (nearest?.ProximityType == ProximityType.Immediate)
-                {
-                    nearest.WasFound = true;
-                    foundBeaconsCount++;
-                    RunOnUiThread(() => foundText.Text = $"ნაპოვნია: {foundBeaconsCount}");
-                }
+		public void OnIBeaconServiceConnect ()
+		{
+			_iBeaconManager.SetRangeNotifier (_rangeNotifier);
+			foreach (var region in _rangingRegion) {
+				_iBeaconManager.RangedRegions.Add (region);
+				_iBeaconManager.StartRangingBeaconsInRegion (region);
+			}
+		}
 
-                if (nearest != null)
-                    RunOnUiThread(() => proximityText.Text = nearest.ProximityType.ToString());
+		void DidRange (object sender, RangeEventArgs e)
+		{
+			if (e.Beacons.Count > 0) {
+				foreach (var beacon in e.Beacons) {
+					var currentBeaconInfo = _beaconsInfo.FirstOrDefault (x => x.Beacon.UUID.ToLower () == beacon.ProximityUuid && x.Beacon.Major == beacon.Major && x.Beacon.Minor == beacon.Minor);
+					if (!(currentBeaconInfo == null) && !currentBeaconInfo.WasFound)
+						switch ((ProximityType)beacon.Proximity) {
+						case ProximityType.Immediate:
+							currentBeaconInfo.ProximityType = ProximityType.Immediate;
+							break;
+						case ProximityType.Near:
+							currentBeaconInfo.ProximityType = ProximityType.Near;
+							break;
+						case ProximityType.Far:
+							currentBeaconInfo.ProximityType = ProximityType.Far;
+							break;
+						case ProximityType.Unknown:
+							currentBeaconInfo.ProximityType = ProximityType.Unknown;
+							break;
+						}
+				}
+				var nearest = _beaconsInfo.Where (x => !x.WasFound && !(x.ProximityType == ProximityType.Unknown)).OrderBy (x => (int)x.ProximityType).FirstOrDefault ();
+				if (nearest?.ProximityType == ProximityType.Immediate) {
+					nearest.WasFound = true;
+					foundBeaconsCount++;
+					RunOnUiThread (() => foundText.Text = $"ნაპოვნია: {foundBeaconsCount}");
+				}
 
-                if (foundBeaconsCount == _beacons.Count)
-                {
-                    RunOnUiThread(() => proximityText.Text = "You found all beacons! Game over!");
-                }
-            }
-            Console.WriteLine();
-        }
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
+				if (nearest != null)
+					RunOnUiThread (() => proximityText.Text = nearest.ProximityType.ToString ());
 
-            _rangeNotifier.DidRangeBeaconsInRegionComplete -= DidRange;
+				if (foundBeaconsCount == _beacons.Count) {
+					RunOnUiThread (() => proximityText.Text = "You found all beacons! Game over!");
+				}
+			}
+			Console.WriteLine ();
+		}
 
-            foreach (var region in _rangingRegion)
-                _iBeaconManager.StopRangingBeaconsInRegion(region);
-            _iBeaconManager.UnBind(this);
-        }
-    }
+		protected override void OnDestroy ()
+		{
+			base.OnDestroy ();
+
+			_rangeNotifier.DidRangeBeaconsInRegionComplete -= DidRange;
+
+			foreach (var region in _rangingRegion)
+				_iBeaconManager.StopRangingBeaconsInRegion (region);
+			_iBeaconManager.UnBind (this);
+		}
+	}
 }
